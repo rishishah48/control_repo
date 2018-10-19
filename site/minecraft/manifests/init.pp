c9eb4ff14f78448cb454654310ab91c644650749
rishishah48 <44096550+rishishah48@users.noreply.github.com>
2018-10-19 18:29:06 -0400

    Create minecraft/manifests/init.pp

---
+++ site/minecraft/manifests/init.pp
@@ -0,0 +1,25 @@
+class minecraft{
+     
+     file{'/opt/minecraft':
+      ensure => directory,
+         }
+     
+     file{'/opt/minecraft/minecraft_server.jar':
+   ensure => present,
+   source => 'https://launcher.mojang.com/v1/objects/fe123682e9cb30031eae351764f653500b7396c9/server.jar',
+         }
+     
+     package{'java':
+   ensure => present,
+         }
+  
+     file{'/opt/minecraft/eula.txt':
+     ensure => file,
+     content => 'eula=true'
+         }
+     
+     file{'/etc/systemd/system/minecraft.service':
+     ensure => file,
+     source => 'puppet:///modules/minecraft/minecraft.service',
+        }
+        }
