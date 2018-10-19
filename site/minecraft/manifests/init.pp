6a399b55a41d6b28e4b080b2a6a5f76bdfa45804
rishishah48 <44096550+rishishah48@users.noreply.github.com>
2018-10-19 19:29:39 -0400

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -22,4 +22,8 @@ class minecraft{
      ensure => file,
      source => 'puppet:///modules/minecraft/minecraft.service',
         }
-        }
+     service{'minecraft':
+     ensure => running,
+     enable => true,
+     }
+   }
