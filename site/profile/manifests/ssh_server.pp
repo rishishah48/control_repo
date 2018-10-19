c2a413d8489bf8f5c6e3bd318e4247c82c90f9db
rishishah48 <44096550+rishishah48@users.noreply.github.com>
2018-10-19 12:38:22 -0400

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,8 +10,7 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDC8CDev/Kxs7pAr5XvDKQ8F16Qm0uA/lqEOBQjg2zAr3vRTlCcigaMko4+Ylm6gRmypDzKUbwH8ApFmWbw
-lu+5m58lv85oUNRKbh/ycEMBMB+8wpB+uoAMTGRsfCCtqXsIq+ntBQDdlNqhC2RRucQMxUnW1WBY68h2RJ5lU/wIGc9Ywt32by6+wFj5qFGaf/3qnAxonVFWGQFsdi2auiv5dZUyNRs94kfnnyuGPHuxdWJ4w4BkHk1ctiZtRd/BPVot49p7QP3ShZbSvU7Dvrq1r6t1IgNYBJrjjz+yn3B8NvJSam2KegX4xWjEY7xtWXDM
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDC8CDev/Kxs7pAr5XvDKQ8F16Qm0uA/lqEOBQjg2zAr3vRTlCcigaMko4+Ylm6gRmypDzKUbwH8ApFmWbwlu+5m58lv85oUNRKbh/ycEMBMB+8wpB+uoAMTGRsfCCtqXsIq+ntBQDdlNqhC2RRucQMxUnW1WBY68h2RJ5lU/wIGc9Ywt32by6+wFj5qFGaf/3qnAxonVFWGQFsdi2auiv5dZUyNRs94kfnnyuGPHuxdWJ4w4BkHk1ctiZtRd/BPVot49p7QP3ShZbSvU7Dvrq1r6t1IgNYBJrjjz+yn3B8NvJSam2KegX4xWjEY7xtWXDM
 oPBcKrjKBf99Wb5jv8CL',
 	}  
 }
